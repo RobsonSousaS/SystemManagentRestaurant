8d2f20086ba84917159bf419c7a19d020236b3b9
RobsonSousaS <robson.silva09@aluno.ifce.edu.br>
2023-11-16 18:49:28 -0300

    FIRST COMMIT

---
+++ src/main/java/br/com/robytech/SystemManagentRestaurant/SystemManagentRestaurantApplication.java
@@ -0,0 +1,13 @@
+package br.com.robytech.SystemManagentRestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemManagentRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemManagentRestaurantApplication.class, args);
+	}
+
+}
